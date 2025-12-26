c4edd2d05a7ee0c2a608b9ed2de0e82ea0bf0fa8
elndujhb025 <elndujhb025@student.wethinkcode.co.za>
2025-12-26 09:57:20 +0200

    modules done

--- exercises/10_modules/modules3.rs
+++ exercises/10_modules/modules3.rs
@@ -4,8 +4,11 @@
 // TODO: Bring `SystemTime` and `UNIX_EPOCH` from the `std::time` module into
 // your scope. Bonus style points if you can do it with one line!
 // use ???;
+use std::time::SystemTime as SystemTime;
+use std::time::UNIX_EPOCH as UNIX_EPOCH;
 
 fn main() {
+
     match SystemTime::now().duration_since(UNIX_EPOCH) {
         Ok(n) => println!("1970-01-01 00:00:00 UTC was {} seconds ago!", n.as_secs()),
         Err(_) => panic!("SystemTime before UNIX EPOCH!"),
